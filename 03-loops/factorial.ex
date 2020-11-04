de30753d954ca28ad04d4895805cbf1a42036b42
Frank Moreno <frankmoreno1993@gmail.com>
2020-11-04 16:26:00 -0500

    factorial

--- 03-loops/factorial.ex
+++ 03-loops/factorial.ex
@@ -1,5 +1,5 @@
 defmodule Factorial do
-  def fac1(n) when is_integer(n) do
+  def classic(n) when is_integer(n) do
     cond do
       n < 2 -> 1
       true -> n * fac1(n - 1)
@@ -23,10 +23,30 @@ defmodule Factorial do
     end
   end
 
-  def fac2(n) do
+  def fac1(n) do
     facp1(n, 0, 1)
+  end
+
+  # fac2(n) = Acc(n, k) * fac2(n - k)
+  # Acc(n, k) = n * (n-1) * ... * (n - k + 1)
+  # Acc(n, k) = Acc(n, k-1) * (n - k + 1)
+  def fac2(n) do
+    cond do
+      n < 1 -> 1
+      true -> facp2(n - 1, n)
+    end
+  end
+
+  defp facp2(0, acc) do
+    acc
+  end
+
+  defp facp2(n, acc) do
+    facp2(n - 1, acc * n)
   end
 end
 
-IO.puts(Factorial.fac1(6))
-IO.puts(Factorial.fac2(5))
+IO.puts(Factorial.classic(6))
+IO.puts(Factorial.fac1(5))
+IO.puts(Factorial.fac2(0))
+IO.puts(Factorial.fac2(1))
